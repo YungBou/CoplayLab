72b75e06ffa3c3dfa2ee2d875973cf08facf3f5d
Jay Bury <buryj15@gmail.com>
2024-04-24 18:17:48 +0200

    resultats et apercu visible par tous

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,10 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
-import { bootstrapExtra } from "@workadventure/scripting-api-extra";
-
 let currentPopup: any = undefined;
 let config = WA.state.loadVariable("config");
 
-// Create an iframe element
-const iframe = document.createElement("iframe");
-iframe.src = "https://example.com";
-iframe.style.width = "100%";
-iframe.style.height = "100%";
-iframe.style.border = "none";
-
 // Waiting for the API to be ready
 WA.onInit().then(() => {
-  // console.log("Scripting API ready");
-  // console.log("Player tags: ", WA.player.tags);
-
   WA.room.area.onEnter("clock").subscribe(() => {
     const today = new Date();
     const time = today.getHours() + ":" + today.getMinutes();
@@ -49,19 +37,11 @@ WA.onInit().then(() => {
     searchWebsite.close();
   });
 
-  // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
-  bootstrapExtra().then(() => {
-    // console.log("Scripting API Extra ready");
-  });
-  //   .catch((e) => console.error(e));
-});
-//   .catch((e) => console.error(e));
-
-function closePopup() {
-  if (currentPopup !== undefined) {
-    currentPopup.close();
-    currentPopup = undefined;
+  function closePopup() {
+    if (currentPopup !== undefined) {
+      currentPopup.close();
+      currentPopup = undefined;
+    }
   }
-}
-
+});
 export {};
